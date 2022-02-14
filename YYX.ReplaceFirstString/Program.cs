88a87837f2c5ae25ff15a93a25be0b4b5264df18
Andy Yin <ayin@aptean.com>
2022-02-14 17:48:12 +0800

    Regex create a long loop

--- YYX.ReplaceFirstString/Program.cs
+++ YYX.ReplaceFirstString/Program.cs
@@ -1,2 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text.RegularExpressions;
+
 Console.WriteLine("Hello, World!");
+
+String userName = "^(([a-z])+.)+[A-Z]([a-z])+$";
+String password = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa!";
+Regex testPassword = new Regex(userName);
+Match match = testPassword.Match(password);
+if (match.Success)
+{
+    Console.WriteLine("Do not include name in password.");
+}
+else
+{
+    Console.WriteLine("Good password.");
+}
+
+Console.ReadKey();
